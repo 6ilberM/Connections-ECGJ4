19f422722ad30cc0966c7b375b00565c5548a391
6ilberM <6ilberm@gmail.com>
2019-06-02 13:32:29 -0500

    Basic Character Made

--- Vinect/Assets/Scripts/Player/inputManager.cs
+++ Vinect/Assets/Scripts/Player/inputManager.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class inputManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public playerController controller;
+
+    public Animator animator;
+
+    float f_force_Hrzt = 0f;
+
+    bool b_jump;
+    public bool m_jumpEnabled = true;
+
+    void Awake()
     {
-        
+
+        if (controller == null)
+        {
+            controller = GameObject.FindGameObjectWithTag("Player").GetComponent<playerController>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //getaxisRaw if you want -1 0 1 get axis 0.0f 0.5f 1.0f and so on
+        f_force_Hrzt = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(f_force_Hrzt));
+
+        if (Input.GetButtonDown("Jump") && m_jumpEnabled)
+        {
+            b_jump = true;
+           // animator.SetBool("IsJumping", true);
+        }
     }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(f_force_Hrzt * Time.fixedDeltaTime, b_jump);
+        b_jump = false;
+    }
+
 }
--- Vinect/Assets/Scripts/Player/playerController.cs
+++ Vinect/Assets/Scripts/Player/playerController.cs
@@ -6,7 +6,26 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class playerController : MonoBehaviour
 {
+    //References
     Rigidbody2D m_rb;
+    Animator m_anim;
+
+    //Custom Ref For Damping
+    private Vector3 m_Velocity = Vector3.zero;
+    private float m_float;
+
+    //Customizeable
+    [SerializeField] float m_jumpHeight = 5;
+
+    //Scalar for movement
+    [Range(1, 10)] [SerializeField] float m_speed = 3.72f;
+
+    //Scalar For Movement Smoothing
+    [Range(0, .3f)] [SerializeField] float m_ScSmooth = .05f;
+
+    //Variables
+    private bool m_FacingRight;
+
 
     private void Awake()
     {
@@ -23,4 +42,57 @@ public class playerController : MonoBehaviour
     {
 
     }
+    ///Flips Character
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Spin Renderer 
+        SpriteRenderer MyImage = gameObject.GetComponent<SpriteRenderer>();
+        MyImage.flipX = !MyImage.flipX;
+
+    }
+    private void Jump(bool _jump)
+    {
+        if (_jump)
+        {
+            float mygrav = m_rb.gravityScale * Physics2D.gravity.y;
+            var _jumpVelocity = (Mathf.Sqrt(Mathf.Abs(mygrav) * m_jumpHeight * 2.0f));
+
+            m_rb.AddForce(m_rb.transform.up * _jumpVelocity * m_rb.mass, ForceMode2D.Impulse);
+        }
+
+    }
+
+
+    public void Move(float _hSpeed, bool _jump)
+    {
+        // float f_trgS = _hSpeed * m_speed * 100;
+        Vector3 v3_targetVel = new Vector2(_hSpeed * m_speed * 100, m_rb.velocity.y);
+
+        if (_hSpeed > 0 || _hSpeed < 0)
+        {
+            // float targetspeed = Mathf.SmoothDamp(m_rb.velocity.x, f_trgS, ref m_float, m_ScSmooth);
+            // m_rb.velocity = new Vector3(targetspeed, m_rb.velocity.y, 0);
+
+            m_rb.velocity = Vector3.SmoothDamp(m_rb.velocity, v3_targetVel, ref m_Velocity, m_ScSmooth);
+        }
+        Jump(_jump);
+
+        if (_hSpeed > 0 && m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player Right and the player is facing right...
+        else if (_hSpeed < 0 && !m_FacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+    }
+
+
+
 }
