4b3e4ae84144eeb86779e812d95b821b4fb7bc39
6ilberM <6ilberm@gmail.com>
2019-06-02 00:20:43 -0500

    thats a lot of tiles /:

--- Vinect/Assets/Scripts/Player/playerController.cs
+++ Vinect/Assets/Scripts/Player/playerController.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class playerController : MonoBehaviour
 {
+    Rigidbody2D m_rb;
+
+    private void Awake()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
