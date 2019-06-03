e315dd636afdb20d12adf4c6848cf64e72fdf28e
6ilberM <6ilberm@gmail.com>
2019-06-03 18:07:32 -0500

    Wootboot

---
+++ Vinect/Assets/Scripts/Player/MankContr.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class MankContr : inputManager 
+{
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        
+    }
+
+
+    private void Update() {
+        
+    }
+}
